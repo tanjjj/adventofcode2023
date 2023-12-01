cd77e38a8a6e017f1cbd98e6a2babf50567a1a36
tanjjj <jeanytan@gmail.com>
2023-12-01 01:10:39 +0000

    add Day0 part 1

--- src/main/java/Day0.java
+++ src/main/java/Day0.java
@@ -10,10 +10,23 @@ public class Day0 implements DayX {
 
         List<Integer> result = get2020(input);
         System.out.println(result);
+
+        System.out.println(result.get(0) * result.get(1));
     }
 
+    private List<Integer> get2020(List<Integer> input) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            int x = 2020-input.get(i);
+            for(int j = i+1; j < input.size(); j++){
+              if(input.get(j) == x){
+                  result.add(input.get(i));
+                  result.add(input.get(j));
+                  break;
+              }
+            }
+        }
 
-    public List<Integer> get2020(List<Integer> input){
-        return new ArrayList<>();
+        return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Day0 day0 = new Day0();
         day0.run();
 
