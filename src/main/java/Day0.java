050bef1cfd2db51acda4f1a0d52cc6db28b3e01e
tanjjj <jeanytan@gmail.com>
2023-12-01 00:56:46 +0000

    add input parsing

---
+++ src/main/java/Day0.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day0 {
+
+    public static List<Integer> get2020(List<Integer> input){
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/Day0Test.java
@@ -0,0 +1,2 @@
+public class Day0Test {
+}
