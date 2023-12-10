a4d0fd16cbcbf42f70193e91d98f88d9f0e1b725
tanjjj <jeanytan@gmail.com>
2023-12-10 18:19:08 +0000

    add day9 part 1

---
+++ src/main/java/Day9.java
@@ -0,0 +1,56 @@
+import utils.Parser;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+// 2023 puzzle 9
+public class Day9 implements DayX {
+
+    @Override
+    public void run() {
+        List<String> input = Parser.parseInputAsString("day9.txt");
+        List<List<Integer>> histories = input.stream()
+                .map(this::getHistory)
+                .collect(Collectors.toList());
+
+        List<Integer> result = histories.stream()
+                .map(this::getNextHistory)
+                .collect(Collectors.toList());
+
+        System.out.println(result.stream().mapToInt(Integer::intValue).sum());
+    }
+
+    private List<Integer> getHistory(String str) {
+        return Arrays.stream(str.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private int getNextHistory(List<Integer> history) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> current = history;
+        int i = 0;
+        map.put(i, current);
+        while (!current.stream().allMatch(x -> x == 0)) {
+            i++;
+            List<Integer> next = getNextLine(current);
+            map.put(i, next);
+            current = next;
+        }
+
+        int result = 0;
+        for (int r = map.size() - 1 - 1; r >= 0; r--) {
+            List<Integer> rowAbove = map.get(r);
+            result = result + rowAbove.get(rowAbove.size() - 1);
+        }
+        return result;
+    }
+
+    private List<Integer> getNextLine(List<Integer> current) {
+        List<Integer> next = new ArrayList<>();
+        for (int i = 1; i < current.size(); i++) {
+            next.add(current.get(i) - current.get(i - 1));
+        }
+        return next;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        //Day0 day0 = new Day0();
-        //day0.run();
-
-        //Day0B day0B = new Day0B();
-        //day0B.run();
-
-        //Day1 day1 = new Day1();
-        //day1.run();
-
-/*        Day1B day1B = new Day1B();
-        day1B.run();*/
-
-/*        Day2 day2 = new Day2();
-        day2.run();*/
-
-/*        Day2B day2B = new Day2B();
-        day2B.run();*/
-
-/*        Day3 day3 = new Day3();
-        day3.run();*/
-
-/*        Day3B day3B = new Day3B();
-        day3B.run();*/
-
-/*        Day4 day4 = new Day4();
-        day4.run();*/
-
-/*        Day4B day4B = new Day4B();
-        day4B.run();*/
-
-/*        Day5 day5 = new Day5();
-        day5.run();*/
-
-/*        Day5Brute brute = new Day5Brute();
-        brute.run();*/
-
-/*        Day5B day5B = new Day5B();
-        day5B.run();*/
-
-/*        Day6 day6 = new Day6();
-        day6.run();*/
-
-/*        Day6B day6B = new Day6B();
-        day6B.run();*/
-
-/*        Day7 day7 = new Day7();
-        day7.run();*/
-
-/*        Day7B day7B = new Day7B();
-        day7B.run();*/
-
-/*        Day8 day8 = new Day8();
-        day8.run();*/
-
-        Day8B day8B = new Day8B();
-        day8B.run();
+        Day9 day9 = new Day9();
+        day9.run();
     }
 
 }
