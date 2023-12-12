3b8ce836cc9ddcadce92cee81b57e470d95ee9f6
tanjjj <jeanytan@gmail.com>
2023-12-12 00:23:25 +0000

    add day11 part 1

--- src/main/java/Day11.java
+++ src/main/java/Day11.java
@@ -5,6 +5,7 @@ import utils.Parser;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 // 2023 puzzle 11
 public class Day11 implements DayX {
@@ -12,14 +13,19 @@ public class Day11 implements DayX {
 
     @Override
     public void run() {
-        List<String> input = Parser.parseInputAsString("test2");
+        List<String> input = Parser.parseInputAsString("day11.txt");
         map = MapHelper.generateMap(input);
         int maxX = input.get(0).length() - 1;
         int maxY = input.size() - 1;
         expandMap(maxX, maxY);
 
+        List<Coordinate> galaxies = map.entrySet().stream()
+                .filter(entry -> entry.getValue().equals('#'))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
 
-        //System.out.println(result.stream().mapToInt(Integer::intValue).sum());
+        List<Integer> result = getDistances(galaxies);
+        System.out.println(result.stream().mapToInt(Integer::intValue).sum());
     }
 
     private void expandMap(int maxX, int maxY) {
@@ -50,7 +56,6 @@ public class Day11 implements DayX {
         int newMaxX = maxX;
         // double empty column
         for (int c : emptyColumns) {
-            System.out.println("expand column " + c);
             for (int x = newMaxX; x >= c; x--) {
                 for (int y = 0; y <= maxY; y++) {
                     map.put(new Coordinate(x + 1, y), map.get(new Coordinate(x, y)));
@@ -63,7 +68,6 @@ public class Day11 implements DayX {
         int newMaxY = maxY;
         // double empty rows
         for (int r : emptyRows) {
-            System.out.println("expand row " + r);
             for (int y = newMaxY; y >= r; y--) {
                 for (int x = 0; x <= newMaxX; x++) {
                     map.put(new Coordinate(x, y + 1), map.get(new Coordinate(x, y)));
@@ -71,6 +75,22 @@ public class Day11 implements DayX {
             }
             newMaxY++;
         }
+    }
+
+    private List<Integer> getDistances(List<Coordinate> galaxies) {
+        List<Integer> result = new ArrayList<>();
+        if (galaxies.size() == 1) {
+            return new ArrayList<>();
+        }
+
+        Coordinate start = galaxies.get(0);
+        for (int i = 1; i < galaxies.size(); i++) {
+            Coordinate end = galaxies.get(i);
+            result.add(Math.abs(start.x - end.x) + Math.abs(start.y - end.y));
+        }
+
+        result.addAll(getDistances(galaxies.subList(1, galaxies.size())));
+        return result;
     }
 
 }
--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -3,7 +3,6 @@ import utils.MapHelper;
 import utils.Parser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
