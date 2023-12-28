1a3892df0c53ad7f2d6d08bfc1c52b291c1e90e9
tanjjj <jeanytan@gmail.com>
2023-12-28 16:57:32 +0000

    add day14 part 2 brute force - faster

--- src/main/java/Day14BNew.java
+++ src/main/java/Day14BNew.java
@@ -22,23 +22,25 @@ public class Day14BNew implements DayX {
         int remainingCircles = 1000000000;
         while (remainingCircles > 0) {
             tiltNorth(maxX, maxY, map);
+            tiltWest(maxX, maxY, map);
+            tiltSouth(maxX, maxY, map);
+            tiltEast(maxX, maxY, map);
             remainingCircles--;
             System.out.println(remainingCircles);
+        }
+
+        for (int i = 0; i <= maxY; i++) {
+            System.out.println(map.get(i));
         }
 
         int result = calculateLoad(maxX, maxY, map);
         System.out.println("result " + result);
     }
 
-/*    private Map<Coordinate, Character> circle(int maxX, int maxY, Map<Coordinate, Character> map) {
-        return tiltEast(maxX, maxY, tiltSouth(maxX, maxY, tiltWest(maxX, maxY, tiltNorth(maxX, maxY, map))));
-    }*/
-
     private void tiltNorth(int maxX, int maxY, List<char[]> map) {
         for (int x = 0; x <= maxX; x++) {
             for (int y = 0; y <= maxY; y++) {
-                char[] oldColumn = map.get(y);
-                char oldValue = oldColumn[x];
+                char oldValue = map.get(y)[x];
                 if (y == 0 || oldValue == '#' || oldValue == '.') {
                     continue;
                 }
@@ -52,84 +54,68 @@ public class Day14BNew implements DayX {
                 map.get(y)[x] = '.';
                 map.get(tmpY)[x] = oldValue;
             }
-
         }
     }
 
-    /*private Map<Coordinate, Character> tiltWest(int maxX, int maxY, Map<Coordinate, Character> map) {
-        Map<Coordinate, Character> newMap = new HashMap<>();
+    private void tiltWest(int maxX, int maxY, List<char[]> map) {
         for (int y = 0; y <= maxY; y++) {
             for (int x = 0; x <= maxX; x++) {
-                Coordinate coordinate = new Coordinate(x, y);
-                Character oldValue = map.get(coordinate);
+                char oldValue = map.get(y)[x];
                 if (x == 0 || oldValue == '#' || oldValue == '.') {
-                    newMap.put(coordinate, oldValue);
                     continue;
                 }
 
                 // slide
                 int tmpX = x;
-                while (tmpX > 0 && isEmpty(new Coordinate(tmpX - 1, y), newMap)) {
+                while (tmpX > 0 && map.get(y)[tmpX - 1] == '.') {
                     tmpX--;
                 }
-                newMap.put(new Coordinate(x, y), '.');
-                newMap.put(new Coordinate(tmpX, y), oldValue);
+
+                map.get(y)[x] = '.';
+                map.get(y)[tmpX] = oldValue;
             }
-
         }
-
-        return newMap;
     }
 
-    private Map<Coordinate, Character> tiltSouth(int maxX, int maxY, Map<Coordinate, Character> map) {
-        Map<Coordinate, Character> newMap = new HashMap<>();
+    private void tiltSouth(int maxX, int maxY, List<char[]> map) {
         for (int x = 0; x <= maxX; x++) {
             for (int y = maxY; y >= 0; y--) {
-                Coordinate coordinate = new Coordinate(x, y);
-                Character oldValue = map.get(coordinate);
+                char oldValue = map.get(y)[x];
                 if (y == maxY || oldValue == '#' || oldValue == '.') {
-                    newMap.put(coordinate, oldValue);
                     continue;
                 }
 
                 // slide
                 int tmpY = y;
-                while (tmpY < maxY && isEmpty(new Coordinate(x, tmpY + 1), newMap)) {
+                while (tmpY < maxY && map.get(tmpY + 1)[x] == '.') {
                     tmpY++;
                 }
-                newMap.put(new Coordinate(x, y), '.');
-                newMap.put(new Coordinate(x, tmpY), oldValue);
+
+                map.get(y)[x] = '.';
+                map.get(tmpY)[x] = oldValue;
             }
-
         }
-
-        return newMap;
     }
 
-    private Map<Coordinate, Character> tiltEast(int maxX, int maxY, Map<Coordinate, Character> map) {
-        Map<Coordinate, Character> newMap = new HashMap<>();
+    private void tiltEast(int maxX, int maxY, List<char[]> map) {
         for (int y = 0; y <= maxY; y++) {
             for (int x = maxX; x >= 0; x--) {
-                Coordinate coordinate = new Coordinate(x, y);
-                Character oldValue = map.get(coordinate);
+                char oldValue = map.get(y)[x];
                 if (x == maxX || oldValue == '#' || oldValue == '.') {
-                    newMap.put(coordinate, oldValue);
                     continue;
                 }
 
                 // slide
                 int tmpX = x;
-                while (tmpX < maxX && isEmpty(new Coordinate(tmpX + 1, y), newMap)) {
+                while (tmpX < maxX && map.get(y)[tmpX + 1] == '.') {
                     tmpX++;
                 }
-                newMap.put(new Coordinate(x, y), '.');
-                newMap.put(new Coordinate(tmpX, y), oldValue);
+
+                map.get(y)[x] = '.';
+                map.get(y)[tmpX] = oldValue;
             }
-
         }
-
-        return newMap;
-    }*/
+    }
 
     private int calculateLoad(int maxX, int maxY, List<char[]> map) {
         int result = 0;
