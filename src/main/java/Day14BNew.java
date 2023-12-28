b72ab321caccf82738cb23d2e8f881a7bb3f75e9
tanjjj <jeanytan@gmail.com>
2023-12-28 17:07:01 +0000

    add day14 part 2 brute force - faster

--- src/main/java/Day14BNew.java
+++ src/main/java/Day14BNew.java
@@ -11,13 +11,17 @@ import java.util.stream.Collectors;
 public class Day14BNew implements DayX {
     @Override
     public void run() {
-        List<String> input = Parser.parseInputAsString("test.txt");
-        List<char[]> map = input.stream()
-                .map(String::toCharArray)
-                .collect(Collectors.toList());
-
+        List<String> input = Parser.parseInputAsString("day14.txt");
         int maxX = input.get(0).length() - 1;
         int maxY = input.size() - 1;
+        char[][] map = new char[maxX + 1][maxX + 1];
+        List<char[]> tmp = input.stream()
+                .map(String::toCharArray)
+                .collect(Collectors.toList());
+        for (int y = 0; y <= maxY; y++) {
+            map[y] = tmp.get(y);
+        }
+
 
         int remainingCircles = 1000000000;
         while (remainingCircles > 0) {
@@ -29,100 +33,96 @@ public class Day14BNew implements DayX {
             System.out.println(remainingCircles);
         }
 
-        for (int i = 0; i <= maxY; i++) {
-            System.out.println(map.get(i));
-        }
-
         int result = calculateLoad(maxX, maxY, map);
         System.out.println("result " + result);
     }
 
-    private void tiltNorth(int maxX, int maxY, List<char[]> map) {
+    private void tiltNorth(int maxX, int maxY, char[][] map) {
         for (int x = 0; x <= maxX; x++) {
             for (int y = 0; y <= maxY; y++) {
-                char oldValue = map.get(y)[x];
+                char oldValue = map[y][x];
                 if (y == 0 || oldValue == '#' || oldValue == '.') {
                     continue;
                 }
 
                 // slide
                 int tmpY = y;
-                while (tmpY > 0 && map.get(tmpY - 1)[x] == '.') {
+                while (tmpY > 0 && map[tmpY - 1][x] == '.') {
                     tmpY--;
                 }
 
-                map.get(y)[x] = '.';
-                map.get(tmpY)[x] = oldValue;
+                map[y][x] = '.';
+                map[tmpY][x] = oldValue;
             }
         }
     }
 
-    private void tiltWest(int maxX, int maxY, List<char[]> map) {
+    private void tiltWest(int maxX, int maxY, char[][] map) {
         for (int y = 0; y <= maxY; y++) {
             for (int x = 0; x <= maxX; x++) {
-                char oldValue = map.get(y)[x];
+                char oldValue = map[y][x];
                 if (x == 0 || oldValue == '#' || oldValue == '.') {
                     continue;
                 }
 
                 // slide
                 int tmpX = x;
-                while (tmpX > 0 && map.get(y)[tmpX - 1] == '.') {
+                while (tmpX > 0 && map[y][tmpX - 1] == '.') {
                     tmpX--;
                 }
 
-                map.get(y)[x] = '.';
-                map.get(y)[tmpX] = oldValue;
+                map[y][x] = '.';
+                map[y][tmpX] = oldValue;
             }
         }
     }
 
-    private void tiltSouth(int maxX, int maxY, List<char[]> map) {
+    private void tiltSouth(int maxX, int maxY, char[][] map) {
         for (int x = 0; x <= maxX; x++) {
             for (int y = maxY; y >= 0; y--) {
-                char oldValue = map.get(y)[x];
+                char oldValue = map[y][x];
                 if (y == maxY || oldValue == '#' || oldValue == '.') {
                     continue;
                 }
 
                 // slide
                 int tmpY = y;
-                while (tmpY < maxY && map.get(tmpY + 1)[x] == '.') {
+                while (tmpY < maxY && map[tmpY + 1][x] == '.') {
                     tmpY++;
                 }
 
-                map.get(y)[x] = '.';
-                map.get(tmpY)[x] = oldValue;
+                map[y][x] = '.';
+                map[tmpY][x] = oldValue;
             }
         }
     }
 
-    private void tiltEast(int maxX, int maxY, List<char[]> map) {
+    private void tiltEast(int maxX, int maxY, char[][] map) {
         for (int y = 0; y <= maxY; y++) {
             for (int x = maxX; x >= 0; x--) {
-                char oldValue = map.get(y)[x];
+                char oldValue = map[y][x];
                 if (x == maxX || oldValue == '#' || oldValue == '.') {
                     continue;
                 }
 
                 // slide
                 int tmpX = x;
-                while (tmpX < maxX && map.get(y)[tmpX + 1] == '.') {
+                while (tmpX < maxX && map[y][tmpX + 1] == '.') {
                     tmpX++;
                 }
 
-                map.get(y)[x] = '.';
-                map.get(y)[tmpX] = oldValue;
+                map[y][x] = '.';
+                map[y][tmpX] = oldValue;
             }
         }
     }
 
-    private int calculateLoad(int maxX, int maxY, List<char[]> map) {
+    private int calculateLoad(int maxX, int maxY, char[][] map) {
         int result = 0;
         int load = maxY + 1;
         for (int y = 0; y <= maxY; y++) {
             for (int x = 0; x <= maxX; x++) {
-                if (map.get(y)[x] == 'O') {
+                if (map[y][x] == 'O') {
                     result += load;
                 }
             }
